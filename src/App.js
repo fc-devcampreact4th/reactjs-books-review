6d900aa0633ca672e9f1f7600136319b5ed6d1c2
yykdev <assavictory@gmail.com>
2019-07-20 18:01:19 +0900

    과제 기본 구성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Error from "./pages/Error";
 
 function App() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/signin" exact />
-                <Route path="/" exact />
-                <Route />
+                <Route path="/signin" exact component={Signin} />
+                <Route path="/" exact component={Home} />
+                <Route component={Error} />
             </Switch>
         </BrowserRouter>
     );
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Row, Col } from "antd";
+import styled from "styled-components";
+import SigninBg from "../component/SigninBg";
+
+const Page = styled(Row).attrs(() => ({
+    type: "flex",
+    align: "middle"
+}))`
+    height: 100vh;
+`; // <Row type={"flex"} align="middle" style={{height: '100vh'}}
+
+const Content = styled.div`
+    width: 800px;
+    margin-right: auto;
+    margin-left: auto;
+`;
+
+const Title = styled.div`
+    text-align: center;
+    font-family: Roboto;
+    font-size: 40px;
+    font-weight: bold;
+    color: #642828;
+    text-transform: uppercase;
+`;
+
+const Subtitle = styled.div`
+    text-align: center;
+    font-family: Roboto;
+    font-size: 27px;
+    font-weight: bold;
+    text-transform: uppercase;
+`;
+
+const Underline = styled.div`
+    width: 200px;
+    height: 8px;
+    margin-right: auto;
+    margin-left: auto;
+    margin-top: 20px;
+    background: linear-gradient(to right, #803b32, #ddb49b);
+`;
+
+const SigninArea = styled(Row)``;
+
+export default function Signin() {
+    return (
+        <Page>
+            <Col span={24} style={{ height: "100px" }}>
+                <Content>
+                    <Title>MARKTUBE REVIEW SERVICE FOR BOOKS</Title>
+                    <Subtitle>
+                        PLEASE SHARE YOUR OPINION ON WEB DEVELOPMENT BOOKS.
+                    </Subtitle>
+                    <Underline />
+                    <SigninArea>
+                        <SigninBg />
+                        {/* <SigninForm>2</SigninForm> */}
+                    </SigninArea>
+                </Content>
+            </Col>
+        </Page>
+    );
+}
