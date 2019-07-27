c04b76382b49df365c03f9a966d5ec3deac54dd1
yykdev <assavictory@gmail.com>
2019-07-27 17:44:15 +0900

    북 리스트 조회

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Error from "./pages/Error";
+import withAuth from "./utils/withAuth";
 
 function App() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/signin" exact component={Signin} />
-                <Route path="/" exact component={Home} />
+                <Route path="/" exact component={withAuth(Home)} />
                 <Route component={Error} />
             </Switch>
         </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,42 @@
 import React from "react";
+import * as axios from "axios";
 
-export default function Home() {
-    return <>Home</>;
+function Book(props) {
+    return <div>{props.title}</div>;
+}
+
+export default class Home extends React.Component {
+    state = {
+        books: []
+    };
+
+    async componentDidMount() {
+        const { token } = this.props;
+        try {
+            const response = await axios.get(
+                "https://api.marktube.tv/v1/book",
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+            this.setState({
+                books: response.data
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    render() {
+        const { books } = this.state;
+        return (
+            <div>
+                <h1>Home</h1>
+                {books.map(book => (
+                    <Book {...book} key={book.bookId} />
+                ))}
+            </div>
+        );
+    }
 }
